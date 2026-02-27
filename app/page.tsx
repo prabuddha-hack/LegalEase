84791b55f1684e214ecc572c21bc950a3a922b0c
prabuddhaxdev <prabuddha1749@gmail.com>
2026-02-28 00:14:37 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+<div>
+  <h1 className="text-3xl font-bold">Hello World</h1>
+  <Button>Click Me</Button>
+</div>
+  );
+}
